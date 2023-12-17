ad6621e6958fc5b5be5e3eea063c0c312791e8da
LauraSabrin <waila_05@hotmail.com>
2023-12-17 23:10:32 +0100

    js

---
+++ src/js/lorem.js
@@ -0,0 +1,5 @@
+const div = document.querySelector ('.div');
+
+const content = '<h1>Lorem ipsum</h1>'
+
+const div.innerHTML = content;
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
-'use strict';
+// 'use strict';
 
-console.log('>> Ready :)');
+// document.querySelector('h1').innerHTML = '¡Hola mundo!';
+
+// 'use strict';
+// document.querySelector('h1').innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente';
+
+// const myadress = 'Málaga, 29003"';
+
+// let myaddress = 'Málaga, 29003';
+// myaddress = 'Madrid, 28004';
+
+// const titleElement = document.querySelector('.title');
+
+
+
+// const titleElement = document.querySelector('.title');
+
+// titleElement.innerHTML = 'Bienvenida';
+
+// titleElement.innerHTML = titleElement.innerHTML + ' adalaber';
+
+// const titleElement = document.querySelector('.title');
+
+// titleElement.innerHTML = titleElement.innerHTML + ' Mundo';
+
+
+// const titleElement = document.querySelector ('.h2');
+
+// titleElement.innerHTML = titleElement.innerHTML + 'Natalia';
+
+// titleElement.innerHTML = titleElement.innerHTML + 'Laura';
+
